183f2398f7b5df2290f22562702a4f457420a2a6
mylinkedinbreakthrough-lang <mylinkedinbreakthrough@gmail.com>
2026-09-15 22:39:48 +0100

    Update Random_password_generator.py

--- Python-Task3-RandomPasswordGenerator/Random_password_generator.py
+++ Python-Task3-RandomPasswordGenerator/Random_password_generator.py
@@ -1,1 +1,18 @@
+import random
+import string
 
+password_length = int(input("Enter password length: "))
+
+if password_length <= 0:
+    print("Password length must be greater than zero.")
+    exit()
+
+characters = string.ascii_letters + string.digits + string.punctuation
+
+password = ""
+
+for i in range(password_length):
+    character = random.choice(characters)
+    password = password + character
+
+print("Generated password:", password)
